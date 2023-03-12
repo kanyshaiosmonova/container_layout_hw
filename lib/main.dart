8eca62b5785bbd634340ff86791a4328acd701db
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2023-03-13 00:19:10 +0600

    finish the first task

--- lib/main.dart
+++ lib/main.dart
@@ -11,126 +11,129 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Containers Example',
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 245, 244, 244),
         appBar: AppBar(
           title: const Text('Containers Example'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(20),
-              height: 44,
-              width: 236,
-              margin: const EdgeInsets.only(
-                  left: 85, top: 20), // reduced top margin value
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: const [
-                  Icon(Icons.web_asset),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: Text(
-                      'Current weight',
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/weight.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Current weight',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '--kgs',
                       style: TextStyle(
+                        color: Color(0xff898A8D),
                         fontFamily: 'Poppins',
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
-                        color: Colors.red,
                       ),
                     ),
-                  ),
-                  Text(
-                    '--kgs',
-                    style: TextStyle(
-                      color: Color(0x00898A8D),
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/weight.png',
+                      width: 24,
+                      height: 24,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              height: 44,
-              width: 236,
-              margin: const EdgeInsets.only(
-                  left: 85, top: 20), // reduced top margin value
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: const [
-                  Icon(Icons.web_asset),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: Text(
-                      'Target weight',
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Target weight',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '--kgs',
                       style: TextStyle(
+                        color: Color(0xff898A8D),
                         fontFamily: 'Poppins',
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
-                        color: Colors.black,
                       ),
                     ),
-                  ),
-                  Text(
-                    '--kgs',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/height.png',
+                      width: 24,
+                      height: 24,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              height: 44,
-              width: 236,
-              margin: const EdgeInsets.only(
-                  left: 85, top: 20), // reduced top margin value
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/height.svg',
-                    width: 24,
-                    height: 24,
-                  ),
-                  const SizedBox(width: 10),
-                  const Expanded(
-                    child: Text(
-                      'Height',
+                    const SizedBox(width: 10),
+                    const Expanded(
+                      child: Text(
+                        'Height',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      '--cms',
                       style: TextStyle(
+                        color: Color(0xff898A8D),
                         fontFamily: 'Poppins',
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
-                        color: Colors.black,
                       ),
                     ),
-                  ),
-                  const Text(
-                    '--cms',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
